bc65122c658263292f766a26043ccbc7c31631cd
Ogork Tabi <joe@l-agence.digital>
2023-01-10 20:40:07 +0100

    tupples in typescript

---
+++ tupEnums.ts
@@ -0,0 +1,16 @@
+// the tupple will ensure that the data is written in a strict order.
+let tUser: [string, number, boolean]
+
+tUser = ["Havey", 132, true];
+let rgb: [number, number, number] = [255, 123, 112];
+
+type User = [number, string];
+
+const newUser: User = [ 112, 'example@google.com'];
+newUser[1] = "yandex.com";
+
+
+
+
+
+export {}
